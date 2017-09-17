4bcbbfcd9683b50ec5c3ede9036775648e405c47
Dmitry Odintsov <odintsoff@gmail.com>
2017-09-17 14:02:50 +0300

    Separate Component::GetId from util::TemplateCounter
    Added proxy static method GetId to Component that to make it easier to
    check for ID. Also has `static_assert` inside to make sure that class
    is derived from Component.

--- source/util/Types.hpp
+++ source/util/Types.hpp
@@ -1,9 +1,12 @@
 #ifndef XANTHUS_UTIL_TYPES_HPP
 #define XANTHUS_UTIL_TYPES_HPP
 
+#include "util/TemplateCounter.hpp"
+
 #include <cstdint>
 #include <limits>
 #include <memory>
+#include <type_traits>
 
 namespace xanthus
 {
@@ -12,7 +15,17 @@ typedef uint32_t EntityId;
 static EntityId const InvalidEntityId = std::numeric_limits<EntityId>::max();
 
 // Placeholder class for basic RTTI implementation
-struct Component {};
+struct Component
+{
+    template<class C>
+        static inline std::size_t GetId()
+    {
+        static_assert(true == std::is_base_of<Component, C>::value, "C shall be derived from Component");
+
+        return util::TemplateCounter<Component>::GetId<C>();
+    }
+};
+
 typedef std::unique_ptr<Component> ComponentPtr;
 
 }
