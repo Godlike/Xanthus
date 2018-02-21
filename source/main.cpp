dc88b7b6d5ae380eb44a2df3affa86ed45f4353c
Dmitry Odintsov <odintsoff@gmail.com>
2018-02-21 02:03:51 -0500

    Apply Mule logging system

--- source/main.cpp
+++ source/main.cpp
@@ -6,16 +6,52 @@
 
 #include "Application.hpp"
 
+#include <mule/MuleUtilities.hpp>
+#include <mule/Loggers.hpp>
+
 #include <unicorn/Settings.hpp>
+#include <unicorn/Loggers.hpp>
+
+#include <spdlog/sinks/ansicolor_sink.h>
+#include <spdlog/sinks/file_sinks.h>
 
 #include <cstdlib>
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
+    auto ansiSink = std::make_shared<spdlog::sinks::ansicolor_stdout_sink_mt>();
+    ansiSink->set_level(mule::LogLevel::warn);
+
+    auto fileSink = std::make_shared<spdlog::sinks::simple_file_sink_mt>("xanthus.log", true);
+
+    {
+        mule::Loggers::Instance().SetDefaultSettings(
+            mule::Loggers::Settings{
+                std::string()
+                , std::string("%+")
+                , mule::LogLevel::trace
+                , { ansiSink, fileSink }
+            }
+        );
+    }
+
+    mule::MuleUtilities::Initialize();
+
+    {
+        unicorn::Loggers::Instance().SetDefaultSettings(
+            unicorn::Loggers::Settings{
+                std::string()
+                , std::string("%+")
+                , mule::LogLevel::trace
+                , { ansiSink, fileSink }
+            }
+        );
+    }
+
     unicorn::Settings& settings = unicorn::Settings::Instance();
 
-    settings.Init(argc, argv, "sandbox.log");
+    settings.Init(argc, argv);
     settings.SetApplicationName("Sandbox");
 
     xanthus::Application app(settings);
